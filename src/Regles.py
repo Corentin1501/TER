c4f61a5e136483242a525c02b9d34b4619bca92a
Corentin <corentin.cuvelier49@gmail.com>
2024-04-10 09:28:07 +0200

    réarrengement de l'arborescence du projet

---
+++ src/Regles.py
@@ -0,0 +1,110 @@
+from bs4 import BeautifulSoup
+# import cssutils
+
+
+#============ Classe mère ============
+
+class Rule:
+    pass
+
+#============ Existence d'une balise ============
+
+
+class Balise_rule(Rule):
+    balises = []
+
+    def __init__(self, balises):
+        self.balises = balises
+
+    def print_rule(self):
+        out = "\t"
+        for balise in self.balises:
+            out += balise + "  >  "
+        print(out[:-3], end='\n')
+
+    def verif_rule(self, html_content):
+        def verif_recursive(tag, rule_index):
+            if rule_index >= len(self.balises):
+                return True
+            else:
+                current_balise = self.balises[rule_index]
+                next_tags = tag.find_all(current_balise)
+                for next_tag in next_tags:
+                    if verif_recursive(next_tag, rule_index + 1):
+                        return True
+                return False
+
+        tag = BeautifulSoup(html_content, 'html.parser')
+        return verif_recursive(tag, 0)
+    
+
+#============ Verification de la valeur d'une balise ============
+
+
+class Value_rule(Rule):
+    balises = []
+    value = ""
+
+    def __init__(self, balise, value):
+        self.balises = balise
+        self.value = value
+
+    def print_rule(self):
+        out = "\t"
+        for balise in self.balises:
+            out += balise + "  >  "
+        print(out[:-3] + "  ==  '" + self.value, end="'\n")
+
+    def verif_rule(self, html_content):
+        def verif_recursive(tag, rule_index):
+            if rule_index >= len(self.balises):
+                return tag.string == self.value
+            else:
+                current_balise = self.balises[rule_index]
+                next_tags = tag.find_all(current_balise)
+                for next_tag in next_tags:
+                    if verif_recursive(next_tag, rule_index + 1):
+                        return True
+                return False
+
+        tag = BeautifulSoup(html_content, 'html.parser')
+        return verif_recursive(tag, 0)
+
+
+#============ Verification des valeurs des attributs d'une balise ============
+
+
+class Attribute_rule(Rule):
+    balises = []
+    attributs = {}
+
+    def __init__(self, balise, attributs):
+        self.balises = balise
+        self.attributs = attributs
+
+    def print_rule(self):
+        out = "\t"
+        for balise in self.balises:
+            out += balise + "  >  "
+        print(out[:-4] + " is ", self.attributs, end="'\n")
+
+    def verif_rule(self, html_content):
+        def verif_recursive(tag, balise_index):
+            if tag.name == self.balises[balise_index]:
+                if balise_index == 0:
+                    return True
+                else:
+                    return verif_recursive(tag.parent, balise_index - 1)
+            else:
+                return False
+
+        parser = BeautifulSoup(html_content, 'html.parser')
+        tag = parser.find(attrs=self.attributs)  # on cherche tous les tag avec ces attributs
+        
+        # Si aucun tag n'a ces attributs, alors on peut déjà retourner False
+        if tag is None :
+            return False
+        # Sinon, on va regarder si le tag a la bonne hiérarchie
+        else:
+            balise_index = len(self.balises) -1
+            return verif_recursive(tag,balise_index)
--- src/main.py
+++ src/main.py
@@ -1,102 +1,4 @@
-from bs4 import BeautifulSoup
-# import cssutils
-
-
-#============ Classes des règles ============
-
-class Rule:
-    pass
-
-class Balise_rule(Rule):
-    balises = []
-
-    def __init__(self, balises):
-        self.balises = balises
-
-    def print_rule(self):
-        out = "\t"
-        for balise in self.balises:
-            out += balise + "  >  "
-        print(out[:-3], end='\n')
-
-    def verif_rule(self, html_content):
-        def verif_recursive(tag, rule_index):
-            if rule_index >= len(self.balises):
-                return True
-            else:
-                current_balise = self.balises[rule_index]
-                next_tags = tag.find_all(current_balise)
-                for next_tag in next_tags:
-                    if verif_recursive(next_tag, rule_index + 1):
-                        return True
-                return False
-
-        tag = BeautifulSoup(html_content, 'html.parser')
-        return verif_recursive(tag, 0)
-    
-class Value_rule(Rule):
-    balises = []
-    value = ""
-
-    def __init__(self, balise, value):
-        self.balises = balise
-        self.value = value
-
-    def print_rule(self):
-        out = "\t"
-        for balise in self.balises:
-            out += balise + "  >  "
-        print(out[:-3] + "  ==  '" + self.value, end="'\n")
-
-    def verif_rule(self, html_content):
-        def verif_recursive(tag, rule_index):
-            if rule_index >= len(self.balises):
-                return tag.string == self.value
-            else:
-                current_balise = self.balises[rule_index]
-                next_tags = tag.find_all(current_balise)
-                for next_tag in next_tags:
-                    if verif_recursive(next_tag, rule_index + 1):
-                        return True
-                return False
-
-        tag = BeautifulSoup(html_content, 'html.parser')
-        return verif_recursive(tag, 0)
-
-class Attribute_rule(Rule):
-    balises = []
-    attributs = {}
-
-    def __init__(self, balise, attributs):
-        self.balises = balise
-        self.attributs = attributs
-
-    def print_rule(self):
-        out = "\t"
-        for balise in self.balises:
-            out += balise + "  >  "
-        print(out[:-4] + " is ", self.attributs, end="'\n")
-
-    def verif_rule(self, html_content):
-        def verif_recursive(tag, balise_index):
-            if tag.name == self.balises[balise_index]:
-                if balise_index == 0:
-                    return True
-                else:
-                    return verif_recursive(tag.parent, balise_index - 1)
-            else:
-                return False
-
-        parser = BeautifulSoup(html_content, 'html.parser')
-        tag = parser.find(attrs=self.attributs)  # on cherche tous les tag avec ces attributs
-        
-        # Si aucun tag n'a ces attributs, alors on peut déjà retourner False
-        if tag is None :
-            return False
-        # Sinon, on va regarder si le tag a la bonne hiérarchie
-        else:
-            balise_index = len(self.balises) -1
-            return verif_recursive(tag,balise_index)
+from Regles import *
 
 #============ Lecture des fichiers ============
 
@@ -203,9 +105,9 @@ def main():
     # rules_file = input("Fichier de règles : ")
     # html_file = input("Fichier HTML : ")
     # css_file = input("Fichier CSS : ")
-    rules_file = "regles.txt"
-    html_file = "l1_web_cc_2024/index.html"
-    css_file = "l1_web_cc_2024/style.css"
+    rules_file = "src/regles.txt"
+    html_file = "src/index.html"
+    css_file = "src/style.css"
     
     # Lire les contenus des fichiers
     html_content = read_file(html_file)
