f28b8b70bc688a5f67948bee9d292001e4c59f2b
Corentin <corentin.cuvelier49@gmail.com>
2024-04-11 09:55:27 +0200

    regle composée implementée

--- src/Regles.py
+++ src/Regles.py
@@ -3,6 +3,12 @@ import re
 
 # import cssutils
 
+def attrs_to_string(attributs):
+    out = "{"
+    for att, valeur in attributs.items():
+        out += "'" + str(att) + "'='" + str(valeur) + "', "
+    return out[:-2] + "}"
+
 
 #============ Classe mère ============
 
@@ -35,8 +41,6 @@ class Value_rule(Rule):
 
         parser = BeautifulSoup(html_content, 'html.parser')
         tags = parser.find_all(self.balises[-1], string=self.value)  # on cherche le tag avec cette valeur
-
-        print(tags, end="")
 
         # Si aucun tag n'existe, alors on peut déjà retourner False
         if tags is None :
@@ -76,7 +80,6 @@ class Attribute_rule(Rule):
 
         parser = BeautifulSoup(html_content, 'html.parser')
         tags = parser.find_all(self.balises[-1],attrs=self.attributs)  # on cherche le tag avec ces attributs
-        print(tags, end="")
 
         # Si aucun tag n'existe, alors on peut déjà retourner False
         if tags is None :
@@ -92,6 +95,151 @@ class Attribute_rule(Rule):
 
 #============ Regle générale ============
 
+class Value:
+    valeur = ""
+
+    def __init__(self, valeur):
+        self.valeur = valeur
+
+    def set_tag(self, tag):
+        self.tag = tag
+
+    def verif_rule(self, tag):
+        if tag.string == self.valeur:
+            print("\t\t" + tag.string + "   =   " + self.valeur)
+        else:
+            print("\t\t" + tag.string + "   =X   " + self.valeur)
+        return tag.string == self.valeur
+    
+    def to_string(self):
+        return "'" + str(self.valeur) + "'"
+
+class Attribut:
+    attributs = {}
+
+    def __init__(self, attributs):
+        self.attributs = attributs
+
+    def set_tag(self, tag):
+        self.tag = tag
+    
+    def verif_rule(self, tag):
+        # print("\t\tverif attributs : " + str(tag.attrs))
+        for att, valeur in self.attributs.items():
+            if tag.get(att) is not None:
+                if att == 'class':
+                    if valeur not in tag.get(att):
+                        print("\t\t\t" + str(att) + "   =X   " + str(valeur))
+                        return False
+                else:
+                    if tag.get(att) != valeur:
+                        print("\t\t\t" + str(att) + "   =X   " + str(valeur))
+                        return False
+                    else:
+                        print("\t\t\t" + str(att) + "   =   " + str(valeur))
+            else:
+                print("\t\t\t" + str(att) + "   =X   " + str(valeur))
+                return False
+        print("\t\t\t" + str(att) + "   =   " + str(valeur))
+        return True
+        
+    def to_string(self):
+        return attrs_to_string(self.attributs)
+
+class Main_rule:
+    balises = []
+    rule_index = {}
+    numero = 0
+
+    def __init__(self, balises, rule_index, numero):
+        self.balises = balises
+        self.rule_index = rule_index
+        self.numero = numero
+
+    def to_string(self):
+        out = "[" + str(self.numero) + "]   "
+        for balise in self.balises:
+            out += str(balise) + " > "
+        out = out[:-3] + "\n"
+
+        if len(self.rule_index) != 0:
+            for tag_index, rule in self.rule_index.items():
+                # print("tag index =", tag_index)
+                out += "\t\twhere " + str(self.balises[tag_index]) + " is " + rule.to_string() + "\n"
+        return out
+
+    def add_secondary_rule(self, rule, balise_index):
+        self.rule_index[balise_index] = rule
+
+    def verif_hierarchy(self, html_content):
+        def verif_recursive(tag, balise_index):
+            if tag.name == self.balises[balise_index]:
+                if balise_index == 0:
+                    return True
+                else:
+                    return verif_recursive(tag.parent, balise_index - 1)
+            else:
+                return False
+
+        parser = BeautifulSoup(html_content, 'html.parser')
+        tags = parser.find_all(self.balises[-1])  # on cherche tous les tag correspondant à la dernière balise
+        
+        # Si aucun tag n'existe, alors on peut déjà retourner False
+        if tags is None :
+            return False
+        # Sinon, on va regarder si le tag a la bonne hiérarchie
+        else:
+            balise_index = len(self.balises) -1
+            for tag in tags:
+                if verif_recursive(tag,balise_index):
+                    return True
+            return False
+
+    def verif_rule(self, html_content):
+        def verif_recursive(tag, balise_index):
+            # bonne balise ?
+            if tag.name == self.balises[balise_index]:
+
+                # y a t il une regle particulière sur cette balise ?
+                if self.rule_index.get(balise_index) is not None:
+
+                    
+                    if not self.rule_index.get(balise_index).verif_rule(tag):
+                        return False    # s'il est n'est pas respecté, on arrete
+
+                
+                # sommes nous à la dernière balise à vérifier ?
+                if balise_index == 0:
+                    # y a t il une regle particulière sur cette balise ?
+                    if self.rule_index.get(balise_index) is not None:
+                        return self.rule_index.get(balise_index).verif_rule(tag) # on retourne directement le résultat
+                    else:
+                        return True
+                # il reste des balises à vérifier
+                else:
+                    return verif_recursive(tag.parent, balise_index - 1)
+            else:
+                return False # mauvaise balise
+
+        # on fais une premiere verification de la hiérarchie, 
+        # ça evite de vérifier les attributs ou les valeurs alors que les balises sont déjà mauvaises
+        if not self.verif_hierarchy(html_content):
+            print("Hiérarchie des balises n'est pas respectée")
+            return False
+        else:
+            parser = BeautifulSoup(html_content, 'html.parser')
+            tags = parser.find_all(self.balises[-1])  # on cherche tous les tag correspondant à la dernière balise
+            
+            # Si aucun tag n'existe, alors on peut déjà retourner False
+            if tags is None :
+                return False
+            # Sinon, on va regarder si le tag a la bonne hiérarchie
+            else:
+                balise_index = len(self.balises) -1
+                for tag in tags:
+                    if verif_recursive(tag,balise_index):
+                        return True
+                return False
 
 class Big_rule(Rule):
     balises = []
--- src/main.py
+++ src/main.py
@@ -31,6 +31,12 @@ def print_all_rules(regles):
         print("")
     print("--------------------------------------\n")
 
+def print_all_main_rules(regles):
+    print("\n--------------- Règles ---------------\n")
+    for rule in regles:
+        print(rule.to_string())
+    print("--------------------------------------\n")
+
 #============ Identification du type des regles HTML ============
 
 def identify_rules(regles):
@@ -93,6 +99,67 @@ def identify_rules(regles):
 
     return instances_rules
 
+
+def identify_main_rules(rules):
+
+    main_rules = []
+    for rule in rules:
+        current_tags = []   # on va noter chaque balise une à une
+        rule_index = {}     # on va noter les règles liés aux balises
+
+        new_main_rule = Main_rule(current_tags, rule_index, len(main_rules))
+
+        mots = rule.split(" ")
+        i = 0
+        # On parcours chaque mot pour identifier les regles 
+        while i < len(mots):
+            #=========== Attribut ===========
+
+            if mots[i].startswith('['):
+                attributs = {}
+                isLastAttribut = False
+                # On récupère les attributs et leurs valeurs
+                while not isLastAttribut and i < len(mots):
+                    isLastAttribut = mots[i].endswith(']')
+
+                    mot_clean = mots[i].strip('[').strip(']')
+                    attribut = mot_clean.split('=')
+                    attributs[attribut[0]] = attribut[1].strip('"')
+                    i += 1
+    
+                new_rule = Attribut(attributs)
+                new_main_rule.add_secondary_rule(new_rule,len(current_tags)-1)
+
+            #=========== Valeur ===========
+
+            elif mots[i].startswith('"'):   
+
+                value = ""
+                isEndOfValue = False
+                while not isEndOfValue and i < len(mots):
+                    # print("\tcurrent value", mots[i])
+                    isEndOfValue = mots[i].endswith('"')
+                    value += " " + mots[i].strip('"')
+                    i += 1
+
+                new_rule = Value(value[1:]) # on enleve le premier " " de 'value'
+                new_main_rule.add_secondary_rule(new_rule,len(current_tags)-1)
+
+            #=========== Balise ===========
+
+            else :  
+                current_tags.append(mots[i])
+                i += 1
+
+        if not mots[-1].endswith(']') and not mots[-1].endswith('"') and current_tags:
+            new_main_rule.balises = current_tags.copy()
+            main_rules.append(new_main_rule)
+        else:
+            main_rules.append(new_main_rule)
+
+
+    return main_rules
+
 #============ Vérification des regles HTML ============
 
 def verif_all_html_rules(html_content, regles):
@@ -107,8 +174,8 @@ def verif_all_html_rules(html_content, regles):
     if len(rules_not_respected) != 0:
         print("⚠️  Règles non respectées ⚠️ ")
         for rule in rules_not_respected:
-            print(" - ", end="")
-            rule.print_rule()
+            print(" - " + rule.to_string())
+            # rule.print_rule()
 
 #============ Main ============
 
@@ -127,11 +194,15 @@ def main():
 
     regles = read_rules(rules_file)
     
-    html_rules = identify_rules(get_html_rules(regles))
+    # html_rules = identify_rules(get_html_rules(regles))
 
-    print_all_rules(html_rules)
+    html_main_rules = identify_main_rules(get_html_rules(regles))
 
-    verif_all_html_rules(html_content, html_rules)
+    # print_all_rules(html_rules)
+
+    print_all_main_rules(html_main_rules)
+
+    verif_all_html_rules(html_content, html_main_rules)
 
 if __name__ == "__main__":
     main()
